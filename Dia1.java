26af151fef18c519f615685efc0acf59e245f21c
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2023-10-14 14:39:54 -0300

    Upload Dia1

---
+++ Dia1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; //package (Java.util)
+
+public class Dia1 { //class Main
+    public static void main(String[] args) { //method main/function
+        double numero1, numero2, resultado;
+
+        Scanner numero1Scanner = new Scanner(System.in);
+        Scanner numero2Scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro numero: "); //statements
+        numero1 = numero1Scanner.nextDouble();
+
+        System.out.print("Digite o primeiro numero: "); //statements
+        numero2 = numero2Scanner.nextDouble();
+        resultado = numero1 + numero2;
+        System.out.print("O resultado e: " + resultado);
+
+    }
+}
