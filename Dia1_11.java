42e08edb849878a6d6f59c637b3362e586e4b1c7
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2023-10-14 20:43:29 -0300

    Upload Dia1_11, Dia1_12

---
+++ Dia1_11.java
@@ -0,0 +1,9 @@
+public class Dia1_11 {
+    public static void main(String[] args){
+        int minhaIdade = 28;
+        int salarioMensal = 50000;
+        System.out.println(minhaIdade);
+        System.out.println(salarioMensal);
+
+    }
+}
---
+++ Dia1_12.java
@@ -0,0 +1,18 @@
+public class Dia1_12 {
+    public static void main(String[] args){
+        byte minhaIdade = 28;
+        int salarioMensal = 50_000;
+        long idadeMilisegundos = 788400000000L;
+        double altura = 1.78;
+        char inicial = 'A';
+        boolean eMaiorDeIdade = true;
+        System.out.println(
+                "Minha idade: " + minhaIdade + "\n" +
+                "Salario Mensal: " +salarioMensal + "\n" +
+                "Idade Milisegundos: " + idadeMilisegundos + "\n" +
+                "Altura: " + altura + "\n" +
+                "Inicial: " + inicial  + "\n" +
+                "Eh Maior de Idade: " + eMaiorDeIdade);
+    }
+
+}
