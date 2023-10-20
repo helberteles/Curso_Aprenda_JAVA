53c5b2a90bd8d962f264b0bbf1572bdf411522b7
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2023-10-20 09:38:35 -0300

    Reorganizado o Main

--- br/curso_aprenda_java/dia1/Aula_num10.java
+++
@@ -1,8 +0,0 @@
-package br.curso_aprenda_java.dia1; //packege
-
-public class Aula_num10 { //Class
-    public static void main(String[] args) {
-        System.out.println(""); //Statements
-    } //Method
-
-}
