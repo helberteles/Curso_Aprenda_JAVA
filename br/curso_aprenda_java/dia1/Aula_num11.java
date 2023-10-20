348f99391fd090d1edf92c1e9030112924ae56a4
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2023-10-19 22:38:29 -0300

    Dia 1 - Aula Numero 11

---
+++ br/curso_aprenda_java/dia1/Aula_num11.java
@@ -0,0 +1,10 @@
+package br.curso_aprenda_java.dia1;
+
+public class Aula_num11 {
+    public static void main(String[]args){
+        int minhaIdade = 28;
+        int salarioMensal = 50000;
+        System.out.println(minhaIdade);
+        System.out.println(salarioMensal);
+    }
+}
