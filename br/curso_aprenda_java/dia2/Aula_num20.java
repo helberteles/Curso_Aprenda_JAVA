73443b1db1aaa093008be5e3494ab00d682d7e90
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2023-10-21 00:21:43 -0300

    Dia 2 - Aula Numero 21

--- br/curso_aprenda_java/dia2/Aula_num20.java
+++
@@ -1,8 +0,0 @@
-package br.curso_aprenda_java.dia2;
-
-public class Aula_num20 {
-    public static void main(String[] args){
-        int vNUM = 2;
-
-    }
-}
---
+++ br/curso_aprenda_java/dia2/Aula_num21.java
@@ -0,0 +1,12 @@
+package br.curso_aprenda_java.dia2;
+
+import java.util.Arrays;
+
+public class Aula_num21 {
+    public static void main(String[] args){
+        int[] valores = {10, 20, 15, 30, 40};
+        System.out.println(Arrays.toString(valores));
+        System.out.println(valores.length);
+
+    }
+}
