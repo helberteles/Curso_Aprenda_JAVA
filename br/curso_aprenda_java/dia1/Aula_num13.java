fea242cf33d77eaae2fba257d4bf9a2329d2854d
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2023-10-20 22:51:28 -0300

    Dia 1 - Aula Numero 13

---
+++ br/curso_aprenda_java/dia1/Aula_num13.java
@@ -0,0 +1,14 @@
+package br.curso_aprenda_java.dia1;
+
+import java.util.Locale;
+
+public class Aula_num13 {
+    public static void main(String[] args){
+        byte minhaIdade = 28;
+        String firstName = "Helber " + "Teles";
+        System.out.println(minhaIdade); //sout
+        System.out.println(firstName.toLowerCase()); //apply letras minusculas
+
+    }
+
+}
