4dfa51596d2cbf41f86c77c73d34d541b60eeecb
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2023-10-24 12:10:44 -0300

    Dia 1 - Aula Numero 10 - Atualizada

--- br/curso_aprenda_java/dia1/Aula_num10.java
+++ br/curso_aprenda_java/dia1/Aula_num10.java
@@ -2,7 +2,8 @@ package br.curso_aprenda_java.dia1; //packege
 
 public class Aula_num10 { //Class
     public static void main(String[] args) {
-        System.out.println(""); //Statements
+
+        System.out.println("Olá Mundo"); //Statements
     } //Method
 
 }
