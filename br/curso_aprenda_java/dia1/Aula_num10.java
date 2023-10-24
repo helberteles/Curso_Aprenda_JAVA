6aad326a6a1d5a1ea72479e63a2c3abda0dcc951
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2023-10-24 17:53:07 -0300

    Dia 1 - Aula Numero 10 - Inclusao de novo parametro

--- br/curso_aprenda_java/dia1/Aula_num10.java
+++ br/curso_aprenda_java/dia1/Aula_num10.java
@@ -3,7 +3,7 @@ package br.curso_aprenda_java.dia1; //packege
 public class Aula_num10 { //Class
     public static void main(String[] args) {
 
-        System.out.println("Olá Mundo"); //Statements
+        System.out.println("Olá Mundo Teste"); //Statements
     } //Method
 
 }
